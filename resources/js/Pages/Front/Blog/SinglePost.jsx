e4f340cfe12afae8e676865cac42dc902304c153
Ahmad zaki Alawi <zaki.alawi22@gmail.com>
2024-09-18 00:35:55 +0700

    revert

--- resources/js/Pages/Front/Blog/SinglePost.jsx
+++ resources/js/Pages/Front/Blog/SinglePost.jsx
@@ -303,25 +303,7 @@ const SinglePost = ({ article }) => {
                                 />
                             </div>
 
-                            <div id="ads-article" className="">
-                                <script
-                                    async
-                                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8778037825157711"
-                                    crossorigin="anonymous"
-                                ></script>
-                                <ins
-                                    class="adsbygoogle"
-                                    style="display:block; text-align:center;"
-                                    data-ad-layout="in-article"
-                                    data-ad-format="fluid"
-                                    data-ad-client="ca-pub-8778037825157711"
-                                    data-ad-slot="1595355101"
-                                ></ins>
-                                <script>
-                                    (adsbygoogle = window.adsbygoogle ||
-                                    []).push({});
-                                </script>
-                            </div>
+                            <div id="ads-article" className=""></div>
 
                             <div className="py-1 my-2 border-b-2 border-frontend-dark border-opacity-40"></div>
 
